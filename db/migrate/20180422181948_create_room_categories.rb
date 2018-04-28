bfa93411880d3214cadecc42dc5fdf11afe3a4ee
broadsword007 <ferhan0000@gmail.com>
2018-04-29 01:03:54 +0500

    corrected migrations

--- db/migrate/20180422181948_create_room_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateRoomCategories < ActiveRecord::Migration[5.0]
-  def change
-    create_table :room_categories do |t|
-      t.string :name
-      t.string :description
-      t.timestamps
-    end
-  end
-end
