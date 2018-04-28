b3272a9b2b9895b1f0af11d57c30dff9fd8b80ab
broadsword007 <ferhan0000@gmail.com>
2018-04-29 01:53:39 +0500

    static assets

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module HotelManagementSystem
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
--- db/migrate/20180428155932_add_rating_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddRatingToComments < ActiveRecord::Migration[5.0]
-  def change
-    add_column :comments, :rating, :float
-  end
-end
